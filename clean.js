de27e76f341fbba3b0adbf15c59439a7f5fcd7df
Abhishek <kumarabhishek60877@gmail.com>
2026-06-13 21:43:23 +0530

    minute changes

--- clean.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('stitch_landing.html', 'utf8');
-html = html.replace(/src="data:image\/[^"]+"/g, 'src=""');
-fs.writeFileSync('stitch_landing_clean.html', html);
